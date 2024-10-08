caaa1b5f5d5c23336aa09989d00a0f2c71ff354a
Marc Le Goff <marc.le.goff@etu.imt-nord-europe.fr>
2024-10-08 19:14:57 +0200

    Working TP2-5

--- SitePro/v2/template_menu.php
+++ SitePro/v2/template_menu.php
@@ -1,24 +1,25 @@
-<nav class="menu">
-    <ul>
-        <?php
-            function renderMenuToHTML($currentPageId) {
-                // un tableau qui d\'efinit la structure du site
-                $mymenu = array(
-                    // idPage titre
-                    'index' => 'Accueil' ,
-                    'cv' => 'CV' ,
-                    'projets' => 'Projets',
-                    'infos_techniques' => 'Infos'
-                    );
-                // ...
-                foreach($mymenu as $pageId => $pageParameters) {
-                    $CurrentPageString = "";
-                    if ($currentPageId == $pageId){
-                        $CurrentPageString = ' id="currentpage"';
-                    }
-                    echo ('<li><a' . $CurrentPageString . ' href="' . $pageId . '.php">' . $pageParameters . '</a></li>');
-                }
+<?php
+    function renderMenuToHTML($currentPageId) {
+        // un tableau qui d\'efinit la structure du site
+        $mymenu = array(
+            // idPage titre
+            'index' => 'Accueil' ,
+            'cv' => 'CV' ,
+            'projets' => 'Projets',
+            'infos_techniques' => 'Infos'
+            );
+        // ...
+        
+        echo "<nav class=\"menu\"><ul>";
+
+        foreach($mymenu as $pageId => $pageParameters) {
+            $CurrentPageString = "";
+            if ($currentPageId == $pageId){
+                $CurrentPageString = ' id="currentpage"';
             }
-        ?>
-    </ul>
-</nav>
+            echo ('<li><a' . $CurrentPageString . ' href="' . $pageId . '.php">' . $pageParameters . '</a></li>');
+        }
+        
+        echo "</ul></nav>";
+    }
+?>
