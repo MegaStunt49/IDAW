bfa9d43ef8963486661a7ba1e63ed1ad597f3ec9
Marc Le Goff <marc.le.goff@etu.imt-nord-europe.fr>
2024-10-08 11:21:13 +0200

    PHP heure courante

---
+++ TP2-PHP/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>TP2-PHP</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="firstStyle.css">
+    </head>
+    
+    <body>
+        <h1>Heure</h1>
+        <?php       
+            echo date('h:i:s'). '<br>';
+        ?>
+    </body>
+</html>
