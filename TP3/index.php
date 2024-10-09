86127c18336ff7eb8681a31214a27ec152d8fc42
Marc Le Goff <marc.le.goff@etu.imt-nord-europe.fr>
2024-10-09 11:52:41 +0200

    COOKIES

---
+++ TP3/index.php
@@ -0,0 +1,15 @@
+<form id="style_form" action="index.php" method="GET">
+    <select name="css">
+        <option value="style1">style1</option>
+        <option value="style2">style2</option>
+    </select>
+    <input type="submit" value="Appliquer" />
+</form>
+<?php 
+    if (isset($_GET['css'])){
+        setcookie("style", $_GET["css"], time()+3600);
+    }
+    if (isset($_COOKIE["style"])){
+        echo("<p>".$_COOKIE["style"]."</p>");
+    }
+?>
