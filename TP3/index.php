f19474179041159ace15d8893d4ca1e9dfa9d7d9
Marc Le Goff <marc.le.goff@etu.imt-nord-europe.fr>
2024-10-11 10:12:39 +0200

    COOKIE fix

--- TP3/index.php
+++ TP3/index.php
@@ -1,3 +1,9 @@
+<?php 
+    if (isset($_GET['css'])){
+        $_COOKIE["style"] = $_GET['css'];
+        setcookie("style", $_GET["css"], time()+3600);
+    }
+?>
 <form id="style_form" action="index.php" method="GET">
     <select name="css">
         <option value="style1">style1</option>
@@ -6,9 +12,6 @@
     <input type="submit" value="Appliquer" />
 </form>
 <?php 
-    if (isset($_GET['css'])){
-        setcookie("style", $_GET["css"], time()+3600);
-    }
     if (isset($_COOKIE["style"])){
         echo("<p>".$_COOKIE["style"]."</p>");
     }
--- TP3/login.php
+++ TP3/login.php
@@ -1,3 +1,10 @@
+<form id="style_form" action="index.php" method="GET">
+    <select name="css">
+        <option value="style1">style1</option>
+        <option value="style2">style2</option>
+    </select>
+    <input type="submit" value="Appliquer" />
+</form>
 <form id="login_form" action="connected.php" method="POST">
     <table>
         <tr>
