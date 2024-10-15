60e482178ce90f74cfc3a90cb2d7e969b9a63484
Marc Le Goff <marc.le.goff@etu.imt-nord-europe.fr>
2024-10-15 09:28:26 +0200

    CRUD Final Commit

--- TP4/exo4/users.php
+++ TP4/exo4/users.php
@@ -24,6 +24,10 @@
             $request1->execute();
         }
     }
+    if(isset($_POST['id_del'])){
+        $request2 = $pdo->prepare("DELETE FROM users WHERE users.id = ".$_POST['id_del']);
+        $request2->execute();
+    }
     $request = $pdo->prepare("select * from users");
     // TODO: add your code here
     // retrieve data from database using fetch(PDO::FETCH_OBJ) and
@@ -31,19 +35,44 @@
 
     $request->execute();
     $data = $request->fetch(PDO::FETCH_OBJ);
-    echo '<style>table, th, td {border:1px solid black;}</style><h1>Users</h1><table><tr><th>ID</th><th>Login</th><th>Email</th></tr>';
+    echo '<style>table, th, td {border:1px solid black;}</style><h1>Users</h1><table><tr><th>ID</th><th>Login</th><th>Email</th><th>modification</th></tr>';
     while(!empty($data)) {
-        echo '<tr><td>'.$data->id.'</td><td>'.$data->login.'</td><td>'.$data->email.'</td></tr>';
+        echo '<tr><td>'.$data->id.'</td><td>'.$data->login.'</td><td>'.$data->email.'</td><td><form id="modif_form'.$data->id.'" action="users.php" method="POST"><input type="hidden" name="id_modif" value="'.$data->id.'"><input type="submit" value="modifier" /></form><form id="del_form'.$data->id.'" action="users.php" method="POST"><input type="hidden" name="id_del" value="'.$data->id.'"><input type="submit" value="supprimer" /></form></td></tr>';
         $data = $request->fetch(PDO::FETCH_OBJ);
     }
-    echo '</table>';
+    echo '</table><br>';
+
+    if(isset($_POST['id_modif'])){
+        $request3 = $pdo->prepare("SELECT * FROM users WHERE users.id = ".$_POST['id_modif']);
+        $request3->execute();
+        $data3 = $request3->fetch(PDO::FETCH_OBJ);
+        echo '<h2>Modifications</h2><form id="login_form" action="users.php" method="POST">
+                <table>
+                    <input type="hidden" name="id" value="'.$_POST['id_modif'].'">
+                    <tr>
+                        <th>Login :</th>
+                        <td><input type="text" name="login" value="'.$data3->login.'"></td>
+                    </tr>
+                    <tr>
+                        <th>Email :</th>
+                        <td><input type="text" name="email" value="'.$data3->email.'"></td>
+                    </tr>
+                    <tr>
+                        <th></th>
+                        <td><input type="submit" value="Ajouter" /></td>
+                    </tr>
+                </table>
+            </form><form id="cancel" action="users.php"><input type="submit" value="Annuler" /></form>';
+        $pdo = null;
+        exit;
+    }
 
     /*** close the database connection ***/
     $pdo = null;
 ?>
 <form id="login_form" action="users.php" method="POST">
     <table>
-        <hidden name="id" value="NULL">
+        <input type="hidden" name="id" value="NULL">
         <tr>
             <th>Login :</th>
             <td><input type="text" name="login"></td>
