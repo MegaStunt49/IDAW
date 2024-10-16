fb0dc5b381ec21ac95bca618f25e66dcebe10934
Marc Le Goff <marc.le.goff@etu.imt-nord-europe.fr>
2024-10-16 09:23:18 +0200

    Simple Commit (not working)

--- TP4/exo5/users.php
+++ TP4/exo5/users.php
@@ -17,14 +17,32 @@
     // ==============
     // Responses
     // ==============
+    $input = json_decode(file_get_contents('php://input'), true);
     switch($_SERVER["REQUEST_METHOD"]) {
         case 'GET':
             $result = get_users($pdo);
             setHeaders();
             exit(json_encode($result));
         case 'POST':
-            $request = $pdo->prepare("INSERT INTO users (id, login, email) VALUES (NULL, 'login', 'email')");
-            http_response_code(501);
+            $login = $input['login'];
+            $email = $input['email'];
+            $requestPost = $pdo->prepare("INSERT INTO users (id, login, email) VALUES (NULL, '".$login."', '".$email."')");
+            $requestPost->execute();
+            http_response_code(201);
+        case 'PUT':
+            if (isset($_GET['id'])){
+                $id = $_GET['id'];
+                $login = $input['login'];
+                $email = $input['email'];
+                $requestPut = $pdo->prepare("UPDATE users SET login='".$login."', email='".$email."' WHERE id=".$id);
+                $requestPut->execute();
+                http_response_code(201);
+            }
+        case 'DELETE':
+            $id = $_GET['id'];
+            $requestDelete = $pdo->prepare("DELETE FROM users WHERE id=".$id);
+            $requestDelete->execute();
+            http_response_code(201);
         exit(-1);
     }
 ?>
